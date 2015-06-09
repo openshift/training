99287c9c2373750864994057cce8e192b3f6f71c
Erik M Jacobs <erikmjacobs@gmail.com>
2015-06-08 21:31:26 -0400

    apparently beta3 assemble still works

--- beta4/custom-assemble.sh
+++ beta4/custom-assemble.sh
@@ -1,19 +1,12 @@
-#!/bin/bash -e
-#
-# Default STI assemble script for the 'ruby-20-centos' image.
-#
-
-if [ "$1" = "-h" ]; then
-  exec /opt/ruby/bin/usage
-fi
+#!/bin/bash
 
 function rake_assets_precompile() {
-  [ -f .sti/markers/disable_asset_compilation ] && return
+  [ -n $DISABLE_ASSET_COMPILATION ] && return
   [ ! -f Gemfile ] && return
   [ ! -f Rakefile ] && return
   ! grep " rails " Gemfile.lock >/dev/null && return
   ! grep " execjs " Gemfile.lock >/dev/null && return
-  ! bundle exec "rake -T" | grep "assets:precompile" >/dev/null && return
+  ! bundle exec 'rake -T' | grep "assets:precompile" >/dev/null && return
 
   echo "---> Starting asset compilation."
   bundle exec rake assets:precompile
@@ -24,21 +17,10 @@ source .bashrc
 
 set -e
 
-if [ "$(ls /tmp/artifacts/ 2>/dev/null)" ]; then
-  echo "Restoring build artifacts"
-  mv /tmp/artifacts/* $HOME/.
-fi
-
-APP_RUNTIME_DIR="${HOME}/src"
-APP_SRC_DIR="/tmp/src"
-
 echo "---> Installing application source"
-mkdir -p ${APP_RUNTIME_DIR}
-cp -Rf ${APP_SRC_DIR}/* ${APP_RUNTIME_DIR}/
+cp -Rf /tmp/src/* ./
 
-pushd "$APP_RUNTIME_DIR/${APP_ROOT}" >/dev/null
 echo "---> Building your Ruby application from source"
-
 if [ -f Gemfile ]; then
   ADDTL_BUNDLE_ARGS=""
   if [ -f Gemfile.lock ]; then
@@ -52,12 +34,9 @@ if [ -f Gemfile ]; then
   else
     BUNDLE_WITHOUT=${BUNDLE_WITHOUT:-"development:test"}
   fi
-  # We can't use 'exec' here because it will exit from the script after the
-  # bundle install command finish. Thus the bundle install cannot receive SIGINT
-  # from console (ctrl+c)
-  #
+
   echo "---> Running 'bundle install ${ADDTL_BUNDLE_ARGS}'"
-  bundle install --path ${HOME}/bundle ${ADDTL_BUNDLE_ARGS}
+  bundle install --path ./bundle ${ADDTL_BUNDLE_ARGS}
 
   echo "---> Cleaning up unused ruby gems"
   bundle clean -V
@@ -67,6 +46,6 @@ if [[ "$RAILS_ENV" == "production" || "$RACK_ENV" == "production" ]]; then
   rake_assets_precompile
 fi
 
-# TODO: Add `rake db:migrate` if linked with DB container
+echo "---> CUSTOM STI ASSEMBLE COMPLETE"
 
-popd >/dev/null
+# TODO: Add `rake db:migrate` if linked with DB container
